8b4b67d4d3f6ec48bb4725b502a70c2e25b1a850
Aeek True <aeektrue@gmail.com>
2024-04-15 11:32:31 +0300

    Derive JSONStorage from UserList

--- storage/storage.py
+++ storage/storage.py
@@ -9,109 +9,53 @@ class NotOpenedStorageError(Exception):
 class AlreadyOpenedStorageError(Exception):
     pass
 
-class Storage:
+class FileStorage:
     '''
     store = Storage('/path/to/storage.txt')
-    store.open() # load data to RAM
-    data = store.get_data()
-    # modificate storage
-    store.commit() # write data to RAM
-    store.close()
     '''
-    _opened = False
-    _modified = False
-    data = None
-    _auto_commit = False
-    
-    def open(self):
-        if self._opened:
-            raise AlreadyOpenedStorageError()
-        else:
-            self._opened = True
-            self._load_data()
 
-    def get_data(self):
-        if self._opened:
-            return self.data
-        else:
-            raise NotOpenedStorageError()
+    data = None 
 
-    def _load_data(self):
-        ''' load data from disk'''
-        pass
-
-    def _save_data(self):
-        ''' save data to disk '''
-        pass
-    
-    def commit(self):
-        ''' save changes to disk '''
-        if self._modified:
-            self._save_data()
-            logger.debug('Changes saved.')
-        else:
-            logger.debug('No changes to commit.')
-        self._modified = False
-
-    def close(self):
-        if self._opened:
-            self._opened = False
-            self._modified=False
-            self.data = None
-        else:
-            raise NotOpenedStorageError()
-
-    def modifier(func):
-        def wrapper(self, *args, **kwargs):
-            if not self._opened:
-                raise NotOpenedStorageError()
-            r = func(self, *args, **kwargs)
-            self._modified = True
-            logger.debug('Data modified')
-            if self._auto_commit:
-                self.commit()
-            return r
-        return wrapper
-
-
-class FileStorage(Storage):
     def __init__(self, filepath):
-        super(FileStorage, self).__init__()
         self._filepath = _os.path.abspath(filepath)
         if not _os.path.exists(self._filepath):
-            self._init_file_storage(self._filepath)
-        logger.debug(f'Storage {self._filepath} initialized.')
-    
-    def _init_file_storage(self, filepath):
-        pass
+            self._init_storage_file(self._filepath)
+            logger.debug(f'Storage {self._filepath} initialized.')
+        self.load()
 
-    def get_path(self):
-        return self._filepath
+    def _init_storage_file(self, filepath):
+        '''initialize empty storage file'''
+        raise NotImplementedError
+
+    def load(self):
+        ''' load data from disk'''
+        raise NotImplementedError
+
+    def save(self):
+        ''' save data to disk '''
+        raise NotImplementedError
 
 
 class JSONStorage(FileStorage, UserList):
     def __init__(self, filepath):
         super(JSONStorage, self).__init__(filepath)
     
-    def _init_file_storage(self, filepath):
+    def _init_storage_file(self, filepath):
         with open(filepath, 'w') as f:
             _json.dump([], f)
 
-    def _load_data(self):
+    def load(self):
         with open(self._filepath, 'r') as file:
             self.data = _json.load(file)
 
-    @Storage.modifier
-    def append(self, element: dict):
-        self.data.append(element)
-    
+    def save(self): 
+        with open(self._filepath, 'w') as file:
+            _json.dump(self.data, file)
+
     def get_all(self, factory=None):
         if factory is None:
             return self.data
         else:
             return [factory(**record) for record in self.data]
 
-    def _save_data(self): 
-        with open(self._filepath, 'w') as file:
-            _json.dump(self.data, file)
 
--- test.py
+++ test.py
@@ -4,14 +4,10 @@ from storage.storage import JSONStorage
 from loguru import logger
 
 logger.add(sys.stdout, format='[<lvl>{level}</>] - {message}')
-st = JSONStorage('test.json')
 
+st = JSONStorage('test.json')
 def test_modification():
-    st.open()
-    print('Content:', st.get_data())
-    st.append({'lol': 22})
-    print('Content:', st.get_data())
-    st.commit()
-    st.close()
+    st.append('kek')
+    st.save()
 
 test_modification()
